f5d712945271a458886dbf0549a74489b7e9668e
aalicki <aalicki@gmail.com>
2024-01-30 21:53:14 -0500

    - Fixed bug with progress bars not showing.

--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -1,3 +1,10 @@
-<div class="h-{{$height}} w-full rounded-full {{$type}}">
-    <div class="h-{{$height}} {{$innerBarBg}} rounded-full" style="width: {{$completion}}%"></div>
+<div class="flex w-full h-{{$height}} {{$type}} rounded-full overflow-hidden"
+     aria-valuenow="{{$height}}"
+     aria-valuemin="0"
+     aria-valuemax="100">
+
+    <div
+        class="flex flex-col rounded-full overflow-hidden {{$innerBarBg}}"
+        style="width: {{$completion}}%">
+    </div>
 </div>
--- src/View/Components/ProgressBar.php
+++ src/View/Components/ProgressBar.php
@@ -2,6 +2,7 @@
 
 namespace Aalicki\FusionCoreUI\View\Components;
 
+use Aalicki\FusionCoreUI\Fusion;
 use Illuminate\View\Component;
 
 class ProgressBar extends Component
@@ -13,17 +14,17 @@ class ProgressBar extends Component
     public ?int $height;
 
     public function __construct(
-        $type       = 'default',
-        $innerBarBg = 'default',
+        $type       = null,
+        $innerBarBg = null,
         $completion = 0,
-        $height     = 4,
+        $height     = 2,
     ) {
         $this->type = match ($type) {
-            'warning'   => 'bg-red-700/70 text-red-300/90 border-red-300/60',
-            'success'   => 'bg-green-700/70 text-green-300/90 border-green-300/60',
-            'info'      => 'bg-blue-700/70 text-blue-300/90 border-blue-400/60',
-            'notice'    => 'bg-yellow-700/70 text-yellow-300/90 border-yellow-300/60',
-            default     => 'bg-cyan-700/80 text-cyan-300/80 border-cyan-300/60',
+            'warning'   => 'bg-red-700/30',
+            'success'   => 'bg-green-700/70',
+            'info'      => 'bg-blue-700/70',
+            'notice'    => 'bg-yellow-700/70',
+            default     => 'bg-cyan-700/80',
         };
 
         $this->innerBarBg = match ($innerBarBg) {
@@ -31,7 +32,7 @@ class ProgressBar extends Component
             'success'   => 'bg-green-600',
             'info'      => 'bg-blue-600',
             'notice'    => 'bg-yellow-600',
-            default     => 'bg-cyan-400',
+            default     => 'bg-cyan-600',
         };
 
         $this->completion   = $completion;

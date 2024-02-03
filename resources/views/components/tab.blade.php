4ff515cec1a6d836299dd32c7fd8a47d4fefe233
aalicki <aalicki@gmail.com>
2024-02-02 20:45:06 -0500

    - The Tab component now inherits color of Tabs component.

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -1,5 +1,5 @@
 <a
-    class="hover:border-cyan-400 hover:text-cyan-200 w-1/4 px-1 text-center cursor-pointer py-2
-            @if($active) border-b-4 border-cyan-400 @else border-transparent border-b-2 @endif">
+    class="hover:border-current hover:text-current w-1/4 px-1 text-center cursor-pointer py-2
+            @if($active) border-b-4 border-current @else border-transparent border-b-2 @endif">
     {{$slot}}
 </a>
--- src/View/Components/Tabs.php
+++ src/View/Components/Tabs.php
@@ -35,18 +35,14 @@ class Tabs extends Component
 
         $this->bg       = match ($bg) {
             'transparent'   => 'border-0 bg-none',
-            'warning'   => 'bg-red-200/20',
-            'success'   => 'bg-green-200/20',
-            'info'      => 'bg-blue-300/20',
-            'notice'    => 'bg-yellow-300/20',
-            default     => 'bg-cyan-300/20',
+            default         => 'bg-cyan-300/20',
         };
         $this->classes  = $classes;
         $this->type     = match ($type) {
-            'warning'   => 'text-red-300/90  border-red-300/60',
-            'success'   => 'text-green-300/90 border-green-300/60',
-            'info'      => 'text-blue-300/90 border-blue-400/60',
-            'notice'    => 'text-yellow-300/90 border-yellow-300/60',
+            'warning'   => 'text-red-300/90 bg-red-300/40 border-red-300/60',
+            'success'   => 'text-green-300/90 bg-green-300/40 border-green-300/60',
+            'info'      => 'text-blue-300/90 bg-blue-300/40 border-blue-400/60',
+            'notice'    => 'text-yellow-300/90 bg-yellow-300/40 border-yellow-300/60',
             default     => 'text-cyan-300/90 border-cyan-300/60',
         };
     }

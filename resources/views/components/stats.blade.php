4ea057e20db9fe6546f581bde6103310ef6b4f6e
aalicki <aalicki@gmail.com>
2024-02-05 20:23:50 -0500

    - Fixed bug with spacing on stat panels.

--- resources/views/components/stats.blade.php
+++ resources/views/components/stats.blade.php
@@ -3,7 +3,7 @@
         <div class="mx-auto max-w-2xl lg:max-w-none">
 
             <dl
-                class="grid grid-cols-1 gap-{{$spacing}} overflow-hidden rounded-xl
+                class="grid grid-cols-1 gap-x-{{$spacing}} overflow-hidden rounded-xl
                         text-center sm:grid-cols-{{$columns}} lg:grid-cols-{{$columns}}">
                 {{$panels}}
             </dl>
